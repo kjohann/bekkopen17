63ebce08e7bd3cc8430ec110d5066e364a5af4b2
kjohann <kris-90@live.no>
2017-02-06 16:20:31 +0100

    React router

---
+++ src/containers/NotFound.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default () => (<div>
+  <img src="https://mi-od-live-s.legocdn.com/r/www/-/media/portal%20v2010/errors/404%20mainstage%20image.jpg?l.r2=1953967734" alt="" />
+</div>);
--- src/index.js
+++ src/index.js
@@ -1,24 +1,37 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { createStore, applyMiddleware, compose } from 'redux'
+import { routerMiddleware, syncHistoryWithStore  } from 'react-router-redux';
 import { Provider } from 'react-redux'
 import App from './containers/App'
+import NotFound from './containers/NotFound';
 import reducer from './reducers'
 import 'todomvc-app-css/index.css'
 import createLogger from 'redux-logger';
 
-const middleware = applyMiddleware(createLogger({
-  predicate: () => true
-}));
+const middleware = applyMiddleware(
+  routerMiddleware(browserHistory),
+  createLogger({
+    predicate: () => true
+  }
+));
 
 const store = createStore(reducer, {}, compose(
   middleware,
   window.devToolsExtension ? window.devToolsExtension() : f => f
 ));
 
+const history = syncHistoryWithStore(browserHistory, store);
+
 render(
   <Provider store={store}>
-    <App />
+    <Router history={history}>
+      <Route path="/">
+        <IndexRoute component={App} />
+        <Route path="*" component={NotFound} />
+      </Route>
+    </Router>
   </Provider>,
   document.getElementById('root')
 )
